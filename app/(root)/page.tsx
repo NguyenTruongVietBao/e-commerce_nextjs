487a8d00c1dab535e9dc8065af2a1fe4357ec922
NguyenTruongVietBao <nguyentvbao2403@gmail.com>
2025-01-16 13:06:56 +0700

    init project header footer

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Home",
+};
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
+}
